2d1e3e63ebd714d45a0fbde99c9bd8c800b23f3c
Markus Byremo <markuswb77@gmail.com>
2024-08-26 08:17:28 +0200

    Implemented simple signup for user

---
+++ src/main/java/com/crypto/trading/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.crypto.trading.controller;
+
+import com.crypto.trading.modal.User;
+import com.crypto.trading.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+
+        User newUser = new User();
+        newUser.setFullName(user.getFullName());
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(user.getPassword());
+
+        User savedUser = userRepository.save(newUser);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+}
--- src/main/java/com/crypto/trading/modal/User.java
+++ src/main/java/com/crypto/trading/modal/User.java
@@ -7,6 +7,7 @@ import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
