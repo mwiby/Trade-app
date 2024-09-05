bbdef062656e1c74df9d0f0c0cd2dc8005c33b98
Markus Byremo <markuswb77@gmail.com>
2024-09-05 11:37:54 +0200

    Implemented signIn functionality for existing users - with response token

--- src/main/java/com/crypto/trading/controller/AuthController.java
+++ src/main/java/com/crypto/trading/controller/AuthController.java
@@ -4,12 +4,14 @@ import com.crypto.trading.config.JwtProvider;
 import com.crypto.trading.modal.AuthResponse;
 import com.crypto.trading.modal.User;
 import com.crypto.trading.repository.UserRepository;
+import com.crypto.trading.service.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +25,11 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/signup")
-    public ResponseEntity<AuthResponse> registerUser(@RequestBody User user) {
+    @Autowired
+    private CustomUserDetails customUserDetails;
+
+    @PostMapping("/signUp")
+    public ResponseEntity<AuthResponse> registerUser(@RequestBody User user) throws Exception {
 
 
         User emailExists = userRepository.findByEmail(user.getEmail());
@@ -36,7 +41,8 @@ public class AuthController {
         newUser.setFullName(user.getFullName());
         newUser.setEmail(user.getEmail());
         newUser.setPassword(user.getPassword());
-        User savedUser = userRepository.save(newUser);
+
+        userRepository.save(newUser);
 
         Authentication auth = new UsernamePasswordAuthenticationToken(
                 user.getEmail(),
@@ -55,5 +61,42 @@ public class AuthController {
 
 
         return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signIn")
+    public ResponseEntity<AuthResponse> logIn(@RequestBody User user) throws Exception {
+
+        String userName = user.getEmail();
+        String password = user.getPassword();
+
+        Authentication auth = getAuthenticate(userName, password);
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String jwt = JwtProvider.generateToken(auth);
+
+        AuthResponse res = new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("Login Success");
+
+
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    private Authentication getAuthenticate(String userName, String password) {
+
+        UserDetails userDetails = customUserDetails.loadUserByUsername(userName);
+
+        if(userDetails == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        if (!password.equals(userDetails.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Wrong password");
+        }
+
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 }
