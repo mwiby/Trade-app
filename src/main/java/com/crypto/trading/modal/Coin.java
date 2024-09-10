7ec2a8650d00e10b1dcce6a35db9c583979af888
Markus Byremo <markuswb77@gmail.com>
2024-09-10 11:25:51 +0200

    Added Coin entity for coingecko api

---
+++ src/main/java/com/crypto/trading/modal/Coin.java
@@ -0,0 +1,94 @@
+package com.crypto.trading.modal;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Coin {
+
+    @Id
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("symbol")
+    private String symbol;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("image")
+    private String image;
+
+    @JsonProperty("current_price")
+    private Long currentPrice;
+
+    @JsonProperty("market_cap")
+    private Long marketCap;
+
+    @JsonProperty("market_cap_rank")
+    private int marketCapRank;
+
+    @JsonProperty("fully_diluted_valuation")
+    private Long fullyDilutedValuation;
+
+    @JsonProperty("total_volume")
+    private Long totalVolume;
+
+    @JsonProperty("high_24h")
+    private Long high24h;
+
+    @JsonProperty("low_24h")
+    private Long low24h;
+
+    @JsonProperty("price_change_24h")
+    private Long priceChange24h;
+
+    @JsonProperty("price_change_percentage_24h")
+    private double priceChangePercentage24h;
+
+    @JsonProperty("market_cap_change_24h")
+    private Long marketCapChange24h;
+
+    @JsonProperty("market_cap_change_percentage_24h")
+    private double marketCapChangePercentage24h;
+
+    @JsonProperty("circulating_supply")
+    private Long circulatingSupply;
+
+    @JsonProperty("total_supply")
+    private Long totalSupply;
+
+    @JsonProperty("max_supply")
+    private Long maxSupply;
+
+    @JsonProperty("ath")
+    private Long ath;
+
+    @JsonProperty("ath_change_percentage")
+    private double athChangePercentage;
+
+    @JsonProperty("ath_date")
+    private LocalDateTime athDate;
+
+    @JsonProperty("atl")
+    private Long atl;
+
+    @JsonProperty("atl_change_percentage")
+    private double atlChangePercentage;
+
+    @JsonProperty("atl_date")
+    private LocalDateTime atlDate;
+
+    @JsonProperty("last_updated")
+    private LocalDateTime lastUpdated;
+
+    @JsonIgnore
+    private Double roi;
+
+}
