940f0fb85652f11c8264093db261c3263e5884a5
Markus Byremo <markuswb77@gmail.com>
2024-09-16 08:33:37 +0200

    Impl controller asset

--- src/main/java/com/crypto/trading/controller/AssetController.java
+++ src/main/java/com/crypto/trading/controller/AssetController.java
@@ -1,10 +1,14 @@
 package com.crypto.trading.controller;
 
+import com.crypto.trading.modal.Asset;
+import com.crypto.trading.modal.User;
 import com.crypto.trading.service.AssetService;
 import com.crypto.trading.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/asset")
@@ -17,4 +21,32 @@ public class AssetController {
     private UserService userService;
 
 
+    @GetMapping("/{assetId}")
+    public ResponseEntity<Asset> getAssetById(
+            @PathVariable("assetId") Long assetId) throws Exception {
+
+        Asset asset = assetService.getAssetById(assetId);
+        return ResponseEntity.ok().body(asset);
+    }
+
+    @GetMapping("/coin/{coinId}/user")
+    public ResponseEntity<Asset> getAssetByUserIdAndCoinId(
+            @PathVariable String coinId,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+        Asset asset = assetService.findAssetByUserIdAndCoinId(user.getId(), coinId);
+        return ResponseEntity.ok().body(asset);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Asset>> getAssetsForUser(
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+
+        User user = userService.findUserProfileByJwt(jwt);
+        List<Asset> assets = assetService.getUsersAssets(user.getId());
+        return ResponseEntity.ok().body(assets);
+    }
 }
